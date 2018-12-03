cdf61b91384949d52f0dd49d1223edd1fe4c19b1
nitronoid <jackdiver@hotmail.co.uk>
2018-12-03 00:34:38 +0000

    Removed use of mod for speed-up

--- src/decode.cpp
+++ src/decode.cpp
@@ -17,22 +17,31 @@ std::string decode(const span<byte3> _encodedImage,
   std::vector<byte> textArray(_textLength);
 
   // Keep a counter of what bit we need to write to
-  uinteger bit = 0u;
+  uinteger bitCount = 0u;
 
-  uinteger pidx = _pixelOffset + (_characterOffset * nbits) / 3u;
-  uinteger channel = (_characterOffset * nbits) % 3u;
+  // Character offset in number of bits
+  const uinteger bitOffset = _characterOffset * nbits;
+  // Calculate how many pixels we need to offset for this
+  const uinteger bitPxOffset = bitOffset / 3u;
+  // Get the total pixel offset
+  uinteger pidx = _pixelOffset + bitPxOffset;
+  // Calculate the channel offset into out first pixel
+  uinteger channel = bitOffset - bitPxOffset * 3u;
+
   // Iterate over enough pixels to give us the full text
-  for (; bit < numBitsInText; ++pidx)
+  for (; bitCount < numBitsInText; ++pidx)
   {
     // Get the current pixel
     const byte3& pixel = _encodedImage[pidx];
     // Iterate over each channel of the current pixel
-    for (; channel < 3 && bit < numBitsInText; ++channel, ++bit)
+    for (; channel < 3 && bitCount < numBitsInText; ++channel, ++bitCount)
     {
+      const uinteger charCount = bitCount / nbits;
       // get the char we are writing to
-      byte& c = textArray[bit / nbits];
+      byte& c = textArray[charCount];
 
-      c = setBit(c, bit % nbits, getBit(pixel[channel], 0u));
+      // bitCount - charCount * nbits is faster than bitCount % nbits
+      c = setBit(c, bitCount - charCount * nbits, getBit(pixel[channel], 0u));
     }
     channel = 0u;
   }
--- src/encode.cpp
+++ src/encode.cpp
@@ -13,19 +13,25 @@ void encode(span<byte3> _sourceImage,
 {
   constexpr auto nbits = std::numeric_limits<byte>::digits;
 
-  const auto pxOffset = _pixelOffset + (_characterOffset * nbits) / 3; 
-  const auto channelOffset = (_characterOffset * nbits) % 3; 
+  // Character offset in number of bits
+  const uinteger bitOffset = _characterOffset * nbits;
+  // Calculate how many pixels we need to offset for this
+  const uinteger bitPxOffset = bitOffset / 3u;
+  // Get the total pixel offset
+  const uinteger pixelOffset = _pixelOffset + bitPxOffset;
+  // Calculate the channel offset into out first pixel
+  const uinteger channelOffset = bitOffset - bitPxOffset * 3u;
 
-  byte* bptr = &_sourceImage[pxOffset][channelOffset];
+  byte* bptr = &_sourceImage[pixelOffset][channelOffset];
 
   // Iterate over every byte of the text
   for (byte c : _text)
   {
     // iterate over all bits of the char
-    for (byte bit = 0u; bit < nbits; ++bit, ++bptr)
+    for (byte bitCount = 0u; bitCount < nbits; ++bitCount, ++bptr)
     {
       // set the lsb to be the current bit of our char
-      *bptr = setBit(*bptr, 0u, getBit(c, bit));
+      *bptr = setBit(*bptr, 0u, getBit(c, bitCount));
     }
   }
 }
